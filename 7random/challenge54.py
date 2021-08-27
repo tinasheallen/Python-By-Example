c4ade6965246693520c10f5eb14fa4d4b6056bfb
Allen Ndawana <tiphics@yahoo.com>
2021-08-27 16:42:20 +0800

    python by example
    a little book of coding challenges

--- 7random/challenge54.py
+++ 7random/challenge54.py
@@ -1,3 +1,10 @@
 import random
 
-coin_toss = random.choice(["Heads","Tails"])
+user_choice = input("Enter your choice Heads: (h) or Tails: (t)? : ")
+
+coin_toss = random.choice(["h","t"])
+
+if coin_toss == user_choice:
+    print("You win")
+else:
+    print("You Loose Buddy!")
---
+++ 7random/challenge55.py
@@ -0,0 +1,7 @@
+import random
+
+picky = random.randint(1, 5)
+
+user = input("Enter a number :")
+
+if picky == user
